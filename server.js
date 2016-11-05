6f35d6e82702fb43c72e04df0a5cb517833340cc
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-05 10:33:57 +0530

    Adding script on Sat Nov  5 10:33:57 IST 2016 :blush:

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use('/', express.static(__dirname + '/'));
+
 app.get('/',function(req,res) {
 	res.sendFile(__dirname + '/index.html');
 });
