f5ad3f81d5f35b2e4851c46ac22d7a7ec66e79c1
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-05 12:41:00 +0530

    Adding script on Sat Nov  5 12:41:00 IST 2016 :blush:

--- docs/js/main.js
+++
@@ -1,8 +0,0 @@
-if (!!$.prototype.justifiedGallery) {  // if justifiedGallery method is defined
-    var options = {
-        rowHeight: 140,
-        margins: 4,
-        lastRow: 'justify'
-    };
-    $('.article-gallery').justifiedGallery(options);
-}
